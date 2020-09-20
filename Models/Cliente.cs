c6f2cc996a257d5db8bd00ed409712911094eea0
MichaelJrSS <michael.juniorss90@gmai.com>
2020-09-20 17:26:20 -0300

    Adicionar arquivos de projeto.

---
+++ Models/Cliente.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Mako.Models
+{
+    [Table("Clientes")]
+    public class Cliente
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Nome { get; set; }
+
+        [Required]
+        [Display(Name = "Endereço")]
+        public string Endereco { get; set; }
+
+        [Required]
+        public string Telefone { get; set; }
+
+        [Required]
+        [Display(Name = "E-mail")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+
+        [Display(Name = "Data de Nascimento")]
+        [DisplayFormat(DataFormatString = "dd/MM/yyyy", ApplyFormatInEditMode = true)]
+        public DateTime Nascimento { get; set; }
+
+        public bool Ativo { get; set; }
+
+    }
+}
---
+++ Models/MakoContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mako.Models
+{
+    public class MakoContext : DbContext
+    {
+        public MakoContext(DbContextOptions<MakoContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Cliente> Clientes { get; set; }
+
+         
+    }
+
+}
