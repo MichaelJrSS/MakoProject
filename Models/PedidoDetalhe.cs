1c30945074e3cdf54b9aeaf38b03e190c8157dbc
MichaelJrSS <michael.juniorss90@gmai.com>
2020-11-08 16:48:32 -0300

    Criacao das classes de dominio para o Entity   pedido e pedido detalhe

---
+++ Models/PedidoDetalhe.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mako.Models
+{
+
+    [Table("PedidoDetalhes")]
+    public class PedidoDetalhe
+    {
+        public int PedidoDetalheId { get; set; }
+        public int PedidoId { get; set; }
+        public int ProdutoId { get; set; }
+        public int Quantidade { get; set; }
+        public decimal Preco { get; set; }
+        public virtual Produto Produto { get; set; }
+        public virtual Pedido Pedido { get; set; }
+    }
+}
