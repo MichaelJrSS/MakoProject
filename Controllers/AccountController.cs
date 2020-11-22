91c5e51612346840e40c6b5c00c39513a46fa9c5
MichaelJrSS <michael.juniorss90@gmai.com>
2020-11-22 19:01:15 -0300

    Controle de acesso do login viewmodel para carregar a page login logout registrer
    Criada as views dentro da pasta account (login, lougut, register)

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,92 @@
+﻿using Mako.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Mako.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        public AccountController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        [HttpGet]
+        public IActionResult Login(string returnUrl)
+        {
+            return View(new LoginViewModel()
+            {
+                ReturnUrl = returnUrl
+            });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel loginVM)
+        {
+            if (!ModelState.IsValid)
+                return View(loginVM);
+
+            var user = await _userManager.FindByNameAsync(loginVM.UserName);
+
+            if (user != null)
+            {
+                var result = await _signInManager.PasswordSignInAsync(user, loginVM.Password, false, false);
+                if (result.Succeeded)
+                {
+                    if (string.IsNullOrEmpty(loginVM.ReturnUrl))
+                    {
+                        return RedirectToAction("Index", "Home");
+                    }
+                    return Redirect(loginVM.ReturnUrl);
+                }
+            }
+
+            ModelState.AddModelError("", "Usuário e/ou senha incorretos!!");
+            return View(loginVM);
+        }
+
+
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken] //filtro para marcar o post para passagens de senha, deixando as passagens mais seguras atravez do controle dos tokens
+        public async Task<IActionResult> Register(LoginViewModel registroVM)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new IdentityUser() { UserName = registroVM.UserName };
+                var result = await _userManager.CreateAsync(user, registroVM.Password);
+
+                if (result.Succeeded)
+                {
+                    //// Adiciona o usuário padrão ao perfil Member
+                    /*await _userManager.AddToRoleAsync(user, "Member");
+                    await _signInManager.SignInAsync(user, isPersistent: false);*/
+
+                    //return RedirectToAction("LoggedIn", "Account");
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+            return View(registroVM);
+        }
+
+        public ViewResult LoggedIn() => View();
+
+        [HttpPost]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
+        }
+
+    }
+}
