fbf7a0652e31b2ed6d2ffccaa2752cad52ba2ef7
MichaelJrSS <michael.juniorss90@gmai.com>
2020-11-23 22:54:43 -0300

    Criado a partir do scaffolding as views controllers para o CRUD completo a pagina administrativa que foi a ideia original que tinha mostrado na primeira previa para o MOZART essa pagina de adm eh independente do site e servira apenas para o ADM fazer manuante;áo das categorias e dos produtos, gerenciar pedidos e etc toda a parte de manutencao das principais tabelas do banco de dados relacionadas as compras e aos produtos

---
+++ Areas/Admin/Controllers/AdminCategoriaProdutosController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Mako.Models;
+
+namespace Mako.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class AdminCategoriaProdutosController : Controller
+    {
+        private readonly MakoContext _context;
+
+        public AdminCategoriaProdutosController(MakoContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Admin/AdminCategoriaProdutos
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.CategoriaProduto.ToListAsync());
+        }
+
+        // GET: Admin/AdminCategoriaProdutos/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var categoriaProduto = await _context.CategoriaProduto
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (categoriaProduto == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoriaProduto);
+        }
+
+        // GET: Admin/AdminCategoriaProdutos/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/AdminCategoriaProdutos/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,CategoriaNome,Descricao")] CategoriaProduto categoriaProduto)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(categoriaProduto);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(categoriaProduto);
+        }
+
+        // GET: Admin/AdminCategoriaProdutos/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var categoriaProduto = await _context.CategoriaProduto.FindAsync(id);
+            if (categoriaProduto == null)
+            {
+                return NotFound();
+            }
+            return View(categoriaProduto);
+        }
+
+        // POST: Admin/AdminCategoriaProdutos/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,CategoriaNome,Descricao")] CategoriaProduto categoriaProduto)
+        {
+            if (id != categoriaProduto.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(categoriaProduto);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CategoriaProdutoExists(categoriaProduto.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(categoriaProduto);
+        }
+
+        // GET: Admin/AdminCategoriaProdutos/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var categoriaProduto = await _context.CategoriaProduto
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (categoriaProduto == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoriaProduto);
+        }
+
+        // POST: Admin/AdminCategoriaProdutos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var categoriaProduto = await _context.CategoriaProduto.FindAsync(id);
+            _context.CategoriaProduto.Remove(categoriaProduto);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CategoriaProdutoExists(int id)
+        {
+            return _context.CategoriaProduto.Any(e => e.Id == id);
+        }
+    }
+}
